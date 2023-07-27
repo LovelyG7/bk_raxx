185278dfb0b6fa9aacce0d832ebf3b36d63a8cac
LovelyG7 <nia.gayle@gmail.com>
2023-07-27 04:31:46 -0400

    increment, decrement and clear cart. Plus Buy now button for looks

--- src/Cart.js
+++ src/Cart.js
@@ -23,18 +23,37 @@ const Cart = () => {
                  return( 
                   <tr key ={index}>
                       <td>
-                        <img src={item.img} style={{height: '6rem'}} />
+                        <img src={item.img} alt="selected item" style={{height: '6rem'}} />
                       </td> 
                       <td>{item.title}</td>
                       <td>{item.price}</td>
                       <td>Quantity ({item.quantity})</td>
+                      <td>
+                        <button className='btn btn-info ms-2'
+                        onClick={()=>updateItemQuantity(item.id, item.quantity -1)}
+                        >-</button>
+                        <button className='btn btn-info ms-2'
+                        onClick={()=>updateItemQuantity(item.id, item.quantity +1)}
+                        >+</button>
+                        <button className='btn btn-info ms-2'
+                        onClick={()=> removeItem(item.id)}
+                        >Remove Item</button>
+                      </td>
                     </tr>
                 )
             })}
             </tbody>
           </table>
         </div>
-
+        <div className='col-auto ms-auto'>
+          <h2>Total Price:$ {cartTotal}</h2>
+        </div>
+        <div className='col-auto'>
+          <button className='btn btn-danger m-2'
+          onClick={() => emptyCart()}
+          >Clear Cart</button>
+          <button className='btn btn-primary m-2'>Buy Now</button>
+        </div>  
       </div>
     </section>
   )
