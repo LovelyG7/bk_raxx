ff47ba0f51014ce956815aee1b2b88f647ed32e4
LovelyG7 <nia.gayle@gmail.com>
2023-07-27 03:26:25 -0400

    cart added with no features

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Home from "./Home"
 import './App.css';
-
+import Cart from './Cart';
 function App() {
   return (
     <>
       < Home/>
+      <Cart/>
     </>
   );
 }
